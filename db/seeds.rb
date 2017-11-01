0ab8b6546e3ca6fa183209dd23b11a43e534d017
Christina Lin <christinalin31@gmail.com>
2017-11-01 10:21:35 -0700

    seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,3 @@ LineItem.destroy_all
 Product.create! id: 1, prodName: "Nocturnal", prodDesc: "Strong AF", unitPrice: 18.0
 Product.create! id: 2, prodName: "Black Eye", prodDesc: "Stay up for days!", unitPrice: 18.0
 Product.create! id: 3, prodName: "All-Nighter", prodDesc: "All day, all night!", unitPrice: 18.0
-Product.create! id: 4, prclodName: "Rude Awakening", prodDesc: "Wake up, mofo", unitPrice: 18.0
